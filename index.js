b216ce058d165e6e58aba846685bb68a08774da2
Jerick120 <jerick120@Jericks-MacBook-Pro.local>
2025-02-03 10:59:31 +0530

    fix file path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const shop = require('../shop.json')
+const shop = require('./shop.json')
 
 const writeTo = path.join(__dirname, './descriptions')
 try {
