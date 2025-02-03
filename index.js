1ce919a276e0aba78916d23f4269ac4da341424d
Jerick120 <jerick120@Jericks-MacBook-Pro.local>
2025-02-03 10:20:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const shop = require('./shop.json')
+
+const writeTo = path.join(__dirname, './descriptions')
+try {
+    console.log(`⏳ Creating Descriptions...`)
+    for (const c in shop) {
+        for (const i of shop[c]) {
+            if (!i.desc) continue
+            const obj = {
+                title: i.title,
+                conditions: { trade: i.conditions.trade, consumable: i.consumable },
+                desc: i.desc,
+                icon: `https://cdn.discordapp.com/emojis/${i.emoji}.png`
+            }
+            if (i.conditions.singleUse) obj.conditions.singleUse = true
+            if (i.conditions.fling) obj.conditions.fling = i.conditions.fling
+            fs.writeFileSync(path.join(writeTo, `${i.name}.json`), JSON.stringify(obj, null, 4))
+        }
+    }
+    console.log(' ✅ Successfully Created Descriptions.')
+} catch (e) {
+    console.log('❌ Write Error')
+    console.log(e)
+}
